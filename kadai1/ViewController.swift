7d0c640df5653d52ee74bb85634e9f289bd31591
Na2key <na2key@icloud.com>
2020-10-20 20:47:42 +0900

    Initial Commit

---
+++ kadai1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kadai1
+//
+//  Created by 山本 夏紀 on 2020/10/20.
+//  Copyright © 2020 natsuki.yamamoto2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
